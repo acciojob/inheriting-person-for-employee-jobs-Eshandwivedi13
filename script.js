1eab289053c927bea13f28a36056a4f455405ff0
Eshan Dwivedi <79198616+Eshandwivedi13@users.noreply.github.com>
2024-08-23 21:26:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -14,7 +14,7 @@ class Person{
     return this.#age;
   }
 	greet(){
-		console.log("Hello, my name is " + this.#name +" , I am " + this.#age + " years old.");
+		console.log("Hello, my name is " + this.#name +" and I am " + this.#age + " years old.");
 	}
 }
 
@@ -25,7 +25,7 @@ class Employee extends Person{
 		this.#job = jobTitle;
 	}
 	jobGreet(){
-		console.log("Hello, my name is " + this.name +", I am " + this.age + " years old, and my job title is " + this.#job);
+		console.log("Hello, my name is " + this.name +", I am " + this.age + " years old, and my job title is " + this.#job + ".");
 	}
 }
 
