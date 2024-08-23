35490a76135f94386d7429d28dec7b277f63dff1
Eshan Dwivedi <79198616+Eshandwivedi13@users.noreply.github.com>
2024-08-23 21:19:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
-// complete this js code
-function Person(name, age) {}
+// function Person(name, age) { }
+// function Employee(name, age, jobTitle) {}
+class Person{
+	#name;
+	#age;
+	constructor(name, age){
+		this.#age = age;
+		this.#name = name;
+	}
+  get name(){
+    return this.#name;
+  }
+  get age(){
+    return this.#age;
+  }
+	greet(){
+		console.log("Hello, my name is " + this.#name +" , I am " + this.#age + " years old.");
+	}
+}
 
-function Employee(name, age, jobTitle) {}
+class Employee extends Person{
+	#job;
+	constructor(name, age, jobTitle){
+		super(name, age);
+		this.#job = jobTitle;
+	}
+	jobGreet(){
+		console.log("Hello, my name is " + this.name +", I am " + this.age + " years old, and my job title is " + this.#job);
+	}
+}
 
 // Do not change code below this line
 window.Person = Person;
